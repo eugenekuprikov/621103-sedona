71c69778ba454e69472bcedcc0f41b7533472d08
eugenekuprikov <cualcua@gmail.com>
2018-12-22 23:49:38 +0300

    находим поле для установления фокуса при открытии модального окна

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,8 @@
 var btn = document.querySelector(".button-search");
 var popup = document.querySelector(".modal-search");
 
+var arrival = popup.querySelector("[name=date-in]");
+
 btn.addEventListener("click", function() {
 	popup.classList.toggle("modal-show");
 });
