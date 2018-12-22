3ac2edcefef7f347d20a37d05afa93015419b3d4
eugenekuprikov <cualcua@gmail.com>
2018-12-23 00:19:09 +0300

    добавляем обработчик события для закрытия модального окна по клавише ESC

--- js/popup.js
+++ js/popup.js
@@ -39,3 +39,13 @@ form.addEventListener("submit", function (evt) {
       }
     }
 });
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
