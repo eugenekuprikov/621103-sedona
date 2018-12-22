c2ec9c52eecf7bdd0c0c41e1086550af880478e8
eugenekuprikov <cualcua@gmail.com>
2018-12-22 23:52:08 +0300

    находим форму в разметке и записываем ее в переменную

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
 
 var btn = document.querySelector(".button-search");
 var popup = document.querySelector(".modal-search");
-
+var form = popup.querySelector("form");
 var arrival = popup.querySelector("[name=date-in]");
 
 btn.addEventListener("click", function() {
