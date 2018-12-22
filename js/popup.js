22bc88b2f8f5fec919a37e3e24edf3a2bd4c7d64
eugenekuprikov <cualcua@gmail.com>
2018-12-22 23:47:33 +0300

    вызываем переключатель состояния кнопки вкл./выкл. для модального окна

--- js/popup.js
+++ js/popup.js
@@ -3,5 +3,5 @@ var btn = document.querySelector(".button-search");
 var popup = document.querySelector(".modal-search");
 
 btn.addEventListener("click", function() {
-
+	popup.classList.toggle("modal-show");
 });
