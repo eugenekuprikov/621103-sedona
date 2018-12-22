9b36f755c272c47c80d908a31843822a6bddb415
eugenekuprikov <cualcua@gmail.com>
2018-12-22 23:57:58 +0300

    находим поля с количеством взрослых и детей и записываем их в переменные

--- js/popup.js
+++ js/popup.js
@@ -4,6 +4,8 @@ var popup = document.querySelector(".modal-search");
 var form = popup.querySelector("form");
 var arrival = popup.querySelector("[name=date-in]");
 var departure = popup.querySelector("[name=date-out]");
+var adults = popup.querySelector("[name=number-adults]");
+var children = popup.querySelector("[name=number-children]");
 
 btn.addEventListener("click", function() {
 	popup.classList.toggle("modal-show");
